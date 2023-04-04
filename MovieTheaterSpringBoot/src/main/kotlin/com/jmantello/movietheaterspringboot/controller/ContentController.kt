654dc3bd570e571b53616d94ca5968e73385c7c5
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-04-04 16:00:02 -0700

    complete path for getContent endpoint

--- MovieTheaterSpringBoot/src/main/kotlin/com/jmantello/movietheaterspringboot/controller/ContentController.kt
+++ MovieTheaterSpringBoot/src/main/kotlin/com/jmantello/movietheaterspringboot/controller/ContentController.kt
@@ -1,15 +1,17 @@
 package com.jmantello.movietheaterspringboot.controller
 
+import com.jmantello.movietheaterspringboot.model.Content
+import com.jmantello.movietheaterspringboot.service.ContentService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/content")
-class ContentController {
+class ContentController(private val service: ContentService) {
 
-    @GetMapping()
-    fun getContent(): String {
-        return "A content item"
+    @GetMapping("all")
+    fun getContent(): Collection<Content> {
+        return service.getContent()
     }
 }
